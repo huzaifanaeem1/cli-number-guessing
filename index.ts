bc307f1ba4a101872a8fcb4bcf823b4c7c3e9a80
Huzaifa Naeem <159990985+huzaifanaeem1@users.noreply.github.com>
2024-03-23 00:40:09 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1)
+
+const answer = await inquirer.prompt([
+    {
+       name:  "userguessNumber",
+       type: "number",
+       message: "please guess a number between 1 to 10:",
+    },
+]);
+
+if(answer.userguessNumber === randomNumber){
+    console.log("Congratulation! you guessed right number")
+} else{
+    console.log("You guessed wrong number")
+}
